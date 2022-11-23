17e61135c8662211d4a0eaa553b5df206d855e50
Han Zhai <zhaihan001@gmail.com>
2022-11-22 20:39:43 -0700

    models

--- server/models/Business.js
+++ server/models/Business.js
@@ -1,46 +1,22 @@
 const mongoose = require("mongoose");
-
 const { Schema } = mongoose;
-const bcrypt = require("bcrypt");
 const Order = require("./Order");
 const productSchema = require("./Product");
 
 const businessSchema = new Schema({
-  name: {
+  businessName: {
     type: String,
     required: true,
     trim: true,
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 5,
-  },
-
   orders: [Order.schema],
   products: [productSchema],
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
-// set up pre-save middleware to create password
-userSchema.pre("save", async function (next) {
-  if (this.isNew || this.isModified("password")) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
-  }
+const Business = mongoose.model("Business", businessSchema);
 
-  next();
-});
-
-// compare the incoming password with the hashed password
-userSchema.methods.isCorrectPassword = async function (password) {
-  return await bcrypt.compare(password, this.password);
-};
-
-const User = mongoose.model("User", userSchema);
-
-module.exports = User;
+module.exports = Business;
