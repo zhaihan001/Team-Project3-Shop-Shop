80f49395df1de0a39ccef94b13ff807f51a9726e
Jalenarms1 <105141749+Jalenarms1@users.noreply.github.com>
2022-11-22 18:56:33 -0700

    adding local storage hook

---
+++ client/src/hooks/useLocalStorage.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+export const useLocalStorage = (key, initialValue) => {
+    const [value, setValue] = useState(() => {
+        const items = localStorage.getItem(key);
+        if(!items) return initialValue
+        if(items) {
+            const data = JSON.parse(items);
+
+            return data
+        }
+        if(typeof initialValue === "function"){
+            return initialValue();
+        }
+    })
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(value))
+    }, [key, value])
+
+    return [value, setValue]
+}
