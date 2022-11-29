b94322a8e71544a0eecceb4f19e5c0cfc2f63b63
Mike <mskowron4@yahoo.com>
2022-11-28 20:09:51 -0800

    added context files

---
+++ client/src/contexts/UserContext.js
@@ -0,0 +1,24 @@
+import { useMutation, useQuery } from '@apollo/client';
+import React, { Component, createContext, useContext } from 'react';
+import { ADD_USER, LOGIN_USER } from '../utils/mutations';
+
+export const UserContext = createContext();
+
+export const useUserContext = () => {
+    return useContext(UserContext)
+}
+
+export const UserProvider = ({children}) => {
+
+        const { loading, data: userData } = useQuery(LOGIN_USER);
+        console.log(userData);
+
+        const [newUser, { err, data: newUserData}] = useMutation(ADD_USER);
+
+        return (
+            <UserContext.Provider value={{newUser, userData, newUserData}}>
+                {children}
+            </UserContext.Provider>
+        )
+}
+
