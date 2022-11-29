dd01a5dc253c0ddc4c12f64c90768b2069eca825
Jalenarms1 <105141749+Jalenarms1@users.noreply.github.com>
2022-11-29 08:57:35 -0700

    added resolver for deleting user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -324,7 +324,7 @@ const resolvers = {
 
         let removedShop = await Business.findOneAndDelete({userId: context.user._id})
 
-        return {msg: `user ${contex.user._id} has been removed`}
+        return {msg: `user ${context.user._id} has been removed`}
 
       } catch (error) {
         return error
