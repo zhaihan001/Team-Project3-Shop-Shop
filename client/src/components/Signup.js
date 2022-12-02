6c6aae0cf74f8c3db4f6f5602f627d1646ac9c59
Han Zhai <zhaihan001@gmail.com>
2022-12-01 19:28:24 -0700

    add redirect home page after signup and login

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,21 +1,26 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import { useUserContext } from '../contexts/UserContext';
-import Auth from '../utils/auth';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { useUserContext } from "../contexts/UserContext";
+import Auth from "../utils/auth";
 
 function Signup() {
   const { newUser } = useUserContext();
 
-  const [userForm, setUserForm] = useState({username: '', email: '', password: '', confirmPassword: ''})
+  const [userForm, setUserForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
   const handleFormChange = (e) => {
-    const {name, value} = e.target
-    setUserForm(prev => {
+    const { name, value } = e.target;
+    setUserForm((prev) => {
       return {
         ...prev,
-        [name]: value
-      }
-    })
-  }
+        [name]: value,
+      };
+    });
+  };
 
   const submitNewUserForm = async (e) => {
     e.preventDefault();
@@ -24,45 +29,75 @@ function Signup() {
         variables: {
           username: userForm.username,
           email: userForm.email,
-          password: userForm.password
-        }
-      })
-      console.log(data);
+          password: userForm.password,
+        },
+      });
 
       Auth.login(data.addUser.token);
 
       // navigate to my shop or user account page after
-
-      return data
-      
+      window.location.assign("/");
+      return data;
     } catch (error) {
-      return error
+      return error;
     }
-  }
+  };
 
   return (
     <Container>
-    <SignupForm>
-      <form onSubmit={submitNewUserForm}>
-      <h2>Signup</h2>
-      <label htmlFor='name'>Username:</label>
-      <input value={userForm.username} onChange={handleFormChange} type='text' name='username' id='username'/>
-      <label htmlFor='email'>Email:</label>
-      <input value={userForm.email} onChange={handleFormChange} type='email' name='email' id='email'/>
-      <label htmlFor='password'>Password:</label>
-      <input value={userForm.password} onChange={handleFormChange} type='password' name='password' id='password'/>
-      <label htmlFor='confirmPassword'>Confirm Password:</label>
-      <input value={userForm.confirmPassword} onChange={handleFormChange} type='password' name='confirmPassword' id='confirmPassword' style={{border: userForm.password !== userForm.confirmPassword ? "2px solid red" : ""}}/>
-      {userForm.password !== userForm.confirmPassword && <small>Passwords do not match.</small>}
-      <input type="submit" value="Signup" />
-      <a href="/login">Already have an account? Login Here</a>
-      </form>
-    </SignupForm>
-  </Container>
-  )
+      <SignupForm>
+        <form onSubmit={submitNewUserForm}>
+          <h2>Signup</h2>
+          <label htmlFor="name">Username:</label>
+          <input
+            value={userForm.username}
+            onChange={handleFormChange}
+            type="text"
+            name="username"
+            id="username"
+          />
+          <label htmlFor="email">Email:</label>
+          <input
+            value={userForm.email}
+            onChange={handleFormChange}
+            type="email"
+            name="email"
+            id="email"
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            value={userForm.password}
+            onChange={handleFormChange}
+            type="password"
+            name="password"
+            id="password"
+          />
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input
+            value={userForm.confirmPassword}
+            onChange={handleFormChange}
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+            style={{
+              border:
+                userForm.password !== userForm.confirmPassword
+                  ? "2px solid red"
+                  : "",
+            }}
+          />
+          {userForm.password !== userForm.confirmPassword && (
+            <small>Passwords do not match.</small>
+          )}
+          <input type="submit" value="Signup" />
+          <a href="/login">Already have an account? Login Here</a>
+        </form>
+      </SignupForm>
+    </Container>
+  );
 }
 
-export default Signup
+export default Signup;
 
 const Container = styled.div`
   padding-top: 3vw;
@@ -76,35 +111,33 @@ const Container = styled.div`
     word-spacing: 4px;
     text-underline-offset: 8px;
     margin-bottom: 5vh;
- }
-    
-`
+  }
+`;
 
 const SignupForm = styled.div`
-
-display: flex;
-flex-direction: row;
-align-items: center;
-justify-content: center;
-
-form {
   display: flex;
+  flex-direction: row;
   align-items: center;
   justify-content: center;
-  flex-direction: column;
-  width: 100%;
-  font-size: 16px;
-  padding: 60px;
 
-  input {
-    width: 300px;
-    min-height: 35px;
-    padding: 7px;
-    outline: none;
-    border-radius: 5px;
-    border: 1px solid rgb(123, 145, 171);
-    &:focus {
-      border: 2px solid rgb(144, 57, 57);
+  form {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    width: 100%;
+    font-size: 16px;
+    padding: 60px;
+
+    input {
+      width: 300px;
+      min-height: 35px;
+      padding: 7px;
+      outline: none;
+      border-radius: 5px;
+      border: 1px solid rgb(123, 145, 171);
+      &:focus {
+        border: 2px solid rgb(144, 57, 57);
       }
     }
   }
@@ -140,11 +173,10 @@ form {
       color: black;
     }
   }
-  
+
   // Login link
 
   a {
     padding-top: 20px;
   }
-  
-  `
+`;
