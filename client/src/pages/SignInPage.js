d0e42ce6acb64fbeb3e934c58ab3157b2546677c
Jalenarms1 <105141749+Jalenarms1@users.noreply.github.com>
2022-12-02 23:50:09 -0700

    adding rerouting to login and back to previous url

--- client/src/pages/SignInPage.js
+++ client/src/pages/SignInPage.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
+import { Navigate } from 'react-router-dom'
 import Login from '../components/Login'
 import Signup from '../components/Signup'
+import Auth from '../utils/auth'
 
 export default function SignInPage() {
     const [showSignUp, setShowSignUp] = useState(false)
     
+    if(Auth.loggedIn()){
+        return <Navigate to="/" />
+    }
 
   return (
     <>
