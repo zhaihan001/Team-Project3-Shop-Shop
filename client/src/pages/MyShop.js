89c05d7c3924344423f822a04f6a5e8eb8d1396b
Jalenarms1 <105141749+Jalenarms1@users.noreply.github.com>
2022-12-02 20:18:22 -0700

    added rerouting to login if not signed in trying to view my shops page

--- client/src/pages/MyShop.js
+++ client/src/pages/MyShop.js
@@ -3,10 +3,15 @@ import Usershop from "../components/UserShop";
 import CreateShop from '../components/CreateShop';
 import { useShopContext } from "../contexts/ShopContext";
 import { useUserContext } from "../contexts/UserContext";
+import Auth from "../utils/auth"
+import { Navigate } from "react-router-dom";
 
 function MyShop() {
   const { myShop } = useShopContext();
   // const { userData } = useUserContext();
+  if(!Auth.loggedIn()){
+    return <Navigate to='/login' />
+  }
 
   if (!myShop) {
     return (
